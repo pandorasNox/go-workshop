d0ff420d057fe43c79547cc8a722de1ef4484f7a
Tino Stöckel <tino.stoeckel@turbinekreuzberg.com>
2020-02-20 13:37:03 +0100

    update lesson 08_functions

--- workshop/lessons/08_functions/main.go
+++ workshop/lessons/08_functions/main.go
@@ -17,15 +17,6 @@ func main() {
 	fmt.Println(average(xs))
 }
 
-// func average(xs []float64) float64 {
-// 	panic("Not Implemented")
-// }
-
 func average(xs []float64) float64 {
-	total := 0.0
-	for _, v := range xs {
-		total += v
-	}
-
-	return total / float64(len(xs))
+	panic("Not Implemented")
 }
