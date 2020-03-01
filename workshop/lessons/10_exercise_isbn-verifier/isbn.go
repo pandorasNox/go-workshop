591157eb6238eaee78a87ab7ce2ee7ab891a172c
pandorasNox <itspandorasnox@gmail.com>
2020-03-01 09:27:27 +0100

    add solution

--- workshop/lessons/10_exercise_isbn-verifier/isbn.go
+++ workshop/lessons/10_exercise_isbn-verifier/isbn.go
@@ -1,14 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func main() {
-	fmt.Println(IsValidISBN("3-598-21508-8"))
+	// fmt.Println(IsValidISBN("3-598-21508-8"))
+	// fmt.Println(IsValidISBN("3-598-21508-X"))
 }
 
 // IsValidISBN returns ...
 func IsValidISBN(isbn string) bool {
-	return false
+	// remove all '-'
+	isbn = strings.Replace(isbn, "-", "", -1)
+
+	//check length == 10
+	if len(isbn) != 10 {
+		return false
+	}
+
+	//check every char (without checking the last) isDigit
+	for i := 0; i < 9; i++ {
+		charByte := isbn[i]
+		if !unicode.IsDigit(rune(charByte)) {
+			return false
+		}
+	}
+
+	// check last char is digit or x
+	last := isbn[len(isbn)-1]
+	if !(unicode.IsDigit(rune(last)) || last == 'X') {
+		return false
+	}
+
+	//transform isbn toNumeric representation
+	var numericISBN [10]int
+	for i, v := range isbn {
+		if i == 9 && v == 'X' {
+			numericISBN[i] = 10
+			continue
+		}
+
+		numeric, err := strconv.Atoi(string(v))
+		if err != nil {
+			return false
+		}
+
+		numericISBN[i] = numeric
+	}
+
+	// check numeric isbn logic is valid
+	var betweenResult int = 0
+	for i := range numericISBN {
+		if i == 9 {
+			betweenResult = betweenResult + numericISBN[i]
+			continue
+		}
+		betweenResult = betweenResult + (numericISBN[i] * int(math.Abs(float64(i)-10)))
+	}
+
+	result := betweenResult % 11
+
+	return result == 0
 }
--- workshop/lessons/10_exercise_isbn-verifier/isbn_test.go
+++ workshop/lessons/10_exercise_isbn-verifier/isbn_test.go
@@ -2,6 +2,32 @@ package main
 
 import "testing"
 
+var testCases = []struct {
+	givenISBN   string
+	expected    bool
+	description string
+}{
+	{"", false, "invalid isbn - empty"},
+	{"12", false, "invalid isbn - too short"},
+	{"123456789", false, "invalid isbn - less than 10 characters"},
+	{"123-456-789", false, "invalid isbn - less than 10 characters, with dashes"},
+	{"3-598-21507-x", false, "invalid isbn - small letter"},
+	{"123456789X", true, "invalid isbn"},
+	{"0-8436-1072-7", true, "valid isbn"},
+	{"3-598-21508-9", false, "invalid isbn - check digit"},
+	{"0-306-40615-2", true, "valid isbn - just numbers"},
+	{"3-598-21507-A", false, "check digit is a character other than X"},
+}
+
 func TestIsValidISBN(t *testing.T) {
-	t.Error("TEST_FAILED")
+	for _, v := range testCases {
+		if IsValidISBN(v.givenISBN) != v.expected {
+			t.Error(
+				"TEST_FAILED, description: '", v.description, "' ",
+				"input: '", v.givenISBN, "' ",
+				"evaluated to: '", IsValidISBN(v.givenISBN), "' ",
+				"expected: '", v.expected, "' ",
+			)
+		}
+	}
 }
