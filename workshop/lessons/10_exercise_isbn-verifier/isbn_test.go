143f25f0a3e4385cd3d9f1ad8472d90cf1a445cd
Thomas Kühnel <tkuehnel@mailbox.org>
2020-03-16 12:56:46 +0100

    My attempt to impement ISBN10 validation

--- workshop/lessons/10_exercise_isbn-verifier/isbn_test.go
+++ workshop/lessons/10_exercise_isbn-verifier/isbn_test.go
@@ -2,6 +2,37 @@ package main
 
 import "testing"
 
-func TestIsValidISBN(t *testing.T) {
-	t.Error("TEST_FAILED")
+func TestIsValidISBNShouldPassForValidISBN(t *testing.T) {
+	isbn := "3-598-21508-8"
+	if !IsValidISBN(isbn) {
+		t.Errorf("Expected ISBN %s to be valid", isbn)
+	}
+}
+
+func TestIsValidISBNShouldPassForValidISBNWithNonIntegerCheckDigit(t *testing.T) {
+	isbn := "3-598-21507-X"
+	if !IsValidISBN(isbn) {
+		t.Errorf("Expected ISBN %s to be valid", isbn)
+	}
+}
+
+func TestIsValidISBNShouldFailForTooShortISBN(t *testing.T) {
+	isbn := "3-59-21508-8"
+	if IsValidISBN(isbn) {
+		t.Errorf("Expected ISBN %s to be invalid (too short)", isbn)
+	}
+}
+
+func TestIsValidISBNShouldFailForTooLongISBN(t *testing.T) {
+	isbn := "39-598-21508-8"
+	if IsValidISBN(isbn) {
+		t.Errorf("Expected ISBN %s to be invalid (too long)", isbn)
+	}
+}
+
+func TestIsValidISBNShouldFailForInvalidCheckDigit(t *testing.T) {
+	isbn := "3-598-21508-9"
+	if IsValidISBN(isbn) {
+		t.Errorf("Expected ISBN %s to be invalid due to wrong check digit", isbn)
+	}
 }
