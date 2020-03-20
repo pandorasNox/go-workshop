292741abaa6b31486fd52b3c3349a4eb648be20e
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2020-03-20 19:58:31 +0100

    update lesson 04_types

--- workshop/lessons/04_types/main.go
+++ workshop/lessons/04_types/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
 	//numbers
 	fmt.Println("Numbers:")
-	fmt.Println("1 + 1 =", 1+1)
-	fmt.Println("1 + 1 =", 1.0+1.0)
+	fmt.Println("1 + 1 =", 1+1, reflect.TypeOf(1+1))
+	fmt.Println("1 + 1 =", 1.0+1, reflect.TypeOf(1.1+1))
 
 	//strings
 	fmt.Println("Strings:")
 	fmt.Println(len("Hello World"))
+	fmt.Println(len("ÜHello World"))
+	fmt.Println(len([]rune("ÜHello World")))
 	fmt.Println("Hello World"[1])
 	fmt.Println("Hello " + "World")
 	fmt.Println(`Hell World
@@ -21,6 +24,7 @@ func main() {
 
 	//booleans
 	fmt.Println("Booleans:")
+	fmt.Println(reflect.TypeOf(true))
 	fmt.Println(true && true)
 	fmt.Println(true && false)
 	fmt.Println(true || true)
